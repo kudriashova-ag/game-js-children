a680661214d36c04f1fa2d8a71f0e751ea10936d
Anastacia Kudriashova <kudriashova.ag@gmail.com>
2022-01-20 14:49:50 +0200

    ready

--- script.js
+++ script.js
@@ -18,8 +18,9 @@ let isAlive = setInterval(() => {
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'));
   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'));
 
-  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) { 
+  if (cactusLeft < 10 && cactusLeft > 0 && dinoTop >= 140) { 
     alert('GAME OVER');
+    location.reload()
   }
 
 }, 10)
